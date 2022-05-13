2dec3daaa8e630a7ec36a1e8c6b5569193c19746
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-05-13 23:53:13 +0600

    done

--- index.js
+++ index.js
@@ -3,11 +3,38 @@ const app = express();
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qojw0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ async function run(){
+    try{
+        await client.connect();
+        const serviceCollections=  client.db('doctors_portal').collection('services');
+
+        app.get('/service',async(req,res)=>{
+            const query = {}
+            const cursor = serviceCollections.find(query)
+            const collection = await cursor.toArray();
+            res.send(collection)
+        })
+    }
+    finally{
+
+    }
+ }
+ run().catch(console.dir)
+
+
+
 
 app.listen(port,()=>console.log('My site is running',port))
